e4ed9d081fe2b93d60f6accbc0a4cd180e701bc6
rushii1192 <hrushikeshborakhede1089@gmail.com>
2023-03-21 21:05:36 +0530

    dashba

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
-<<<<<<< Updated upstream
-from . models import DoctorModel
-
-# Register your models here.
-
-admin.site.register(DoctorModel)
-
-=======
 from .models import DoctorModel
 # Register your models here.
 admin.site.register(DoctorModel)
->>>>>>> Stashed changes
